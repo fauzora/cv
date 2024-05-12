fb251a0d94fddf3e50db6bbe4ecd50551b997788
chairil <chairil@jkt1.ebdesk.com>
2024-05-13 05:55:13 +0700

    feat: add section project

--- js/typed_anim.js
+++ js/typed_anim.js
@@ -3,13 +3,67 @@ function animateTyped(documentId, listAnimateString) {
     strings: listAnimateString,
     loop: true,
     typeSpeed: 45,
-    backSpeed: 35,
+    backSpeed: 22.5,
     showCursor: false,
   });
 }
 
 animateTyped("animate-profile-name", [
-  "Chairil Fauzi F.",
   "Frontend Developer",
   "Backend Developer",
 ]);
+
+const wrapper = document.querySelectorAll(".cardWrap");
+
+wrapper.forEach((element) => {
+  let state = {
+    mouseX: 0,
+    mouseY: 0,
+    height: element.clientHeight,
+    width: element.clientWidth,
+  };
+
+  element.addEventListener("mousemove", (ele) => {
+    const card = element.querySelector(".card");
+    const cardBg = card.querySelector(".cardBg");
+    state.mouseX = ele.pageX - element.offsetLeft - state.width / 2;
+    state.mouseY = ele.pageY - element.offsetTop - state.height / 2;
+
+    // parallax angle in card
+    const angleX = (state.mouseX / state.width) * 30;
+    const angleY = (state.mouseY / state.height) * -30;
+    card.style.transform = `rotateY(${angleX}deg) rotateX(${angleY}deg) `;
+
+    // parallax position of background in card
+    const posX = (state.mouseX / state.width) * -40;
+    const posY = (state.mouseY / state.height) * -40;
+    cardBg.style.transform = `translateX(${posX}px) translateY(${posY}px)`;
+  });
+
+  element.addEventListener("mouseout", () => {
+    const card = element.querySelector(".card");
+    const cardBg = card.querySelector(".cardBg");
+    card.style.transform = `rotateY(0deg) rotateX(0deg) `;
+    cardBg.style.transform = `translateX(0px) translateY(0px)`;
+  });
+});
+
+window.addEventListener("scroll", function () {
+  var scrollPosition = window.scrollY || window.pageYOffset;
+  var cvTopIcon = document.querySelector(".cv_top_icon a");
+
+  if (scrollPosition > 100) {
+    cvTopIcon.classList.add("show");
+    cvTopIcon.addEventListener("click", scrollToTop);
+  } else {
+    cvTopIcon.classList.remove("show");
+    cvTopIcon.removeEventListener("click", scrollToTop);
+  }
+});
+
+function scrollToTop() {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+}
